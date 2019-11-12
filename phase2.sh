6ebd78fbdab8d1c55d453ac4d28a9b5105d212e2
michelle-aubin <phiangda@cs.ualberta.ca>
2019-11-12 16:41:40 -0700

    ready

--- phase2.sh
+++ phase2.sh
@@ -2,10 +2,10 @@
 
 sort -u -o terms.txt terms.txt
 sort -u -o emails.txt emails.txt
-#sort -u -o dates.txt dates.txt
-#sort -u -o recs.txt recs.txt
+sort -u -o dates.txt dates.txt
+sort -u -o recs.txt recs.txt
 perl break.pl < terms.txt | db_load -T -t btree te.idx
 perl break.pl < emails.txt | db_load -T -t btree em.idx
-#perl break.pl < dates.txt | db_load -T -t btree da.idx
-#perl break.pl < recs.txt | db_load -T -t hash re.idx
+perl break.pl < dates.txt | db_load -T -t btree da.idx
+perl break.pl < recs.txt | db_load -T -t hash re.idx
 
