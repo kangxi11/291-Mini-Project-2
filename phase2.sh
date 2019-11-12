c082a8ad5006c91cd4e76c1cf5b26e40ea1bb656
michelle-aubin <phiangda@cs.ualberta.ca>
2019-11-12 15:08:34 -0700

    trying to add piping

--- phase2.sh
+++ phase2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sort -u terms.txt
-sort -u email.txt
-sort -u dates.txt
-sort -u recs.txt
-perl break.pl < terms.txt
-perl break.pl < email.txt
-perl break.pl < dates.txt
-perl break.pl < recs.txt
+#sort -u terms.txt
+sort -u -o emails.txt emails.txt
+#sort -u dates.txt
+#sort -u recs.txt
+#perl break.pl < terms.txt
+perl break.pl < emails.txt | db_load -t btree em.idx
+#perl break.pl < dates.txt
+#perl break.pl < recs.txt
 
