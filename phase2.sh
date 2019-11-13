7f090197f651138331a49cab4ae4b6d3fd6990c8
michelle-aubin <phiangda@cs.ualberta.ca>
2019-11-13 14:30:25 -0700

    allow duplicate keys

--- phase2.sh
+++ phase2.sh
@@ -4,8 +4,8 @@ sort -u -o terms.txt terms.txt
 sort -u -o emails.txt emails.txt
 sort -u -o dates.txt dates.txt
 sort -u -o recs.txt recs.txt
-perl break.pl < terms.txt | db_load -T -t btree te.idx
-perl break.pl < emails.txt | db_load -T -t btree em.idx
-perl break.pl < dates.txt | db_load -T -t btree da.idx
-perl break.pl < recs.txt | db_load -T -t hash re.idx
+perl break.pl < terms.txt | db_load -c duplicates=1 -T -t btree te.idx
+perl break.pl < emails.txt | db_load -c duplicates=1 -T -t btree em.idx
+perl break.pl < dates.txt | db_load -c duplicates=1 -T -t btree da.idx
+perl break.pl < recs.txt | db_load -c duplicates=1 -T -t hash re.idx
 
