f998910a4cf6d6684f35c07f9fa941b93140d89d
kangxi11 <rkang2009@gmail.com>
2019-11-15 11:51:45 -0500

    preprocessor working on

--- queries.py
+++ queries.py
@@ -1,5 +1,59 @@
 from bsddb3 import db
 
+def searchTerms(subj, body):
+    DB_File = "te.idx"
+    database = db.DB()
+    database.open(DB_File)
+    cur = database.cursor()
+
+    subj_rows = []
+    body_rows = []
+
+    if subj is not None:
+        if '%' not in subj:
+            query = 's-'+subj
+            iter = cur.set(query.encode("utf-8"))
+
+            if iter is not None:
+                while iter[0].decode("utf-8").find(query) != -1:
+                    subj_rows.append(iter[1].decode("utf-8"))
+                    iter = cur.next()
+        else:
+            query = 's-'+subj[:-1]
+            iter = cur.set_range(query.encode("utf-8"))
+            
+            while iter[0].decode("utf-8")[:len(query)].find(query) != -1:
+                subj_rows.append(iter[1].decode("utf-8"))
+                iter = cur.next()
+
+
+
+    if body is not None:
+        if '%' not in body:
+            query = 'b-'+body
+            iter = cur.set(query.encode("utf-8"))
+
+            if iter is not None:
+                while iter[0].decode("utf-8").find(query) != -1:
+                    body_rows.append(iter[1].decode("utf-8"))
+                    iter = cur.next()
+        else:
+            query = 'b-'+body[:-1]
+            iter = cur.set_range(query.encode("utf-8"))
+            
+            while iter[0].decode("utf-8")[:len(query)].find(query) != -1:
+                body_rows.append(iter[1].decode("utf-8"))
+                iter = cur.next()
+    cur.close()
+    database.close()
+
+    if subj is not None and body is not None:
+        return list(set(subj_rows) & set(body_rows))
+    elif subj is None and body is not None:
+        return body_rows
+    elif subj is not None and body is None:
+        return subj_rows
+
 # returns a list of key value pairs specified by row keys in rows
 def searchRecords(rows):
     database = db.DB()
@@ -42,8 +96,10 @@ def main():
         result = []
         emailPrefixes = ["from", "to", "cc", "bcc"]
         termPrefixes = ["subj", "body"]
-        comparators = ["=", "<=", ">=", "<", ">"]
+        comparators = ["<", ">","<=", ">=",]
         rows = set()
+
+        queries = []
 
         if query == "exit":
             break
@@ -58,7 +114,42 @@ def main():
             for y in x.split(":"):
                 if y != "":
                     result.append(y)
+
         # handle the query
+        while result != []:
+            temp_que = []
+            temp_que.append(result.pop(0))
+
+            if temp_que[0] in emailPrefixes or temp_que[0] in termPrefixes:
+                temp_que.append(result.pop(0))
+            elif temp_que[0].find("date") != -1:
+                # find the inequality sign
+                # sign is either in [0] or the next element
+                sign = None
+                for comp in comparators:
+                    if temp_que[0].find(comp) != -1:
+                        sign = comp
+                if sign == None:
+                    for comp in comparators:
+                        if result[0].find(comp) != -1:
+                            sign = comp
+                if sign == None:
+                    sign = ":"
+
+                # now append the query to the master list
+                temp_que[0] = "date"
+                temp_que.append(sign)
+                # next element is just the sign
+                for comp in comparators:
+                    if result[0] == comp:
+                        result.pop(0)
+                        temp_que.append(result.pop(0))
+                        
+            queries.append(temp_que)
+        
+        print(queries)
+
+
         for word in result:
             first = False
             # get next word
@@ -105,6 +196,7 @@ def main():
                     continue
                 # word is a term to search for in subj and body fields
                 else:
+                    print(word)
                     # ryan implements this part
                     continue
 
