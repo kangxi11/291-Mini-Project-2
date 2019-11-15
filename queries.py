d5ca21cd76da9793c0163dbf23f280b7fab8ff80
michelle-aubin <phiangda@cs.ualberta.ca>
2019-11-14 21:15:00 -0700

    outline for main function done

--- queries.py
+++ queries.py
@@ -1,5 +1,9 @@
 from bsddb3 import db
 
+def searchEmails(field, email):
+    print("field is", field)
+    print("email is", email)
+
 def main():
     print("\nWelcome to the information retrieval system.")
     print("You may enter queries to retrieve records from the database.")
@@ -8,10 +12,74 @@ def main():
     print("To change back to the default output, enter \"output=brief\".")
     print("To exit the system, enter \"exit\".\n")
 
+    # 0 for brief output and 1 for full output
+    setting = 0
+
     while True:
         query = input("Please enter your query: ")
+        result = []
+        emailPrefixes = ["from", "to", "cc", "bcc"]
+        termPrefixes = ["subj", "body"]
+        comparators = ["=", "<=", ">=", "<", ">"]
+
         if query == "exit":
             break
+        if query == "output=brief":
+            setting = 0
+            continue
+        if query == "output=full":
+            setting = 1
+            continue
+        # split the query by whitespace and colons
+        for x in query.split():
+            for y in x.split(":"):
+                if y != "":
+                    result.append(y)
+        # handle the query
+        for word in result:
+            # clear rows list
+            rows = []
+            # get next word
+            if result.index(word) != len(result)-1:
+                next_word = result[result.index(word)+1]
+            # get previous word
+            if result.index(word) != 0:
+                prev_word = result[result.index(word)-1]
+            # check if word is to, from, cc or bcc
+            if word in emailPrefixes:
+                rows = searchEmails(word, next_word)
+            # check if word is subj or body
+            else if word in termPrefixes:
+                rows = searchTerms(word, next_word)
+            # check if word is date
+            else if word[:4] == "date":
+                # daniel handles all the different cases
+            # word is not a prefix
+            else:
+                # word is < > <= >= or =
+                if word in comparators:
+                    continue
+                # word is part of email or term query
+                else if prev_word in emailPrefixes or prev_word in termPrefixes:
+                    continue
+                # word is part of date query
+                else if "date" in prev_word or prev_word in comparators:
+                    continue
+                # word is a term to search for in subj and body fields
+                else:
+                    # ryan implements this part
+
+            # get output from records index and print
+            output = searchRecords(rows)
+            # print full output
+            if setting:
+                print(output)
+            # print brief output
+            else:
+                print(output)
+            # we'll deal with the correct printing later
+
+
 
 
 main()
