b181413c1f832ba520981594c04532d154aa5249
kangxi11 <rkang2009@gmail.com>
2019-11-14 23:23:55 -0500

    subj and body done

--- ryan.py
+++ ryan.py
@@ -1,12 +1,44 @@
 from bsddb3 import db
-DB_File = "te.idx"
-database = db.DB()
-database.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
-database.open(DB_File,None, db.DB_BTREE, db.DB_CREATE)
-cur = database.cursor()
 
-iter = cur.first()
 
-while(iter):
-    print(iter)
-    iter = cur.next()
+def searchTerm(subj, body):
+    DB_File = "te.idx"
+    database = db.DB()
+    database.open(DB_File)
+    cur = database.cursor()
+
+    subj_rows = []
+    body_rows = []
+
+    if subj is not None:
+        query = 's-'+subj
+        iter = cur.set(query.encode("utf-8"))
+
+        if iter is not None:
+            while iter[0].decode("utf-8").find(query) != -1:
+                subj_rows.append(iter[1].decode("utf-8"))
+                iter = cur.next()
+
+    if body is not None:
+        query = 'b-'+body
+        iter = cur.set(query.encode("utf-8"))
+
+        if iter is not None:
+            while iter[0].decode("utf-8").find(query) != -1:
+                body_rows.append(iter[1].decode("utf-8"))
+                iter = cur.next()
+    cur.close()
+    database.close()
+
+    return list(set(subj_rows) & set(body_rows))
+
+
+
+def main():
+    
+    result = searchTerm("check", "the")
+
+    print(result)
+
+
+main()
