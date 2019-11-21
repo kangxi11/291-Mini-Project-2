87eb3bd51b040da4c3de2f0088f2b3c56a881e3b
kangxi11 <rkang2009@gmail.com>
2019-11-21 16:44:50 -0700

    sds

--- queries.py
+++ queries.py
@@ -264,11 +264,11 @@ def main():
                 print("Full record: ", pair[1].decode("utf-8"))
         # print brief output
         else:
-            print ("Row ID       |       Subject Field")
+            print ("Row ID       | Subject Field\n")
             for pair in output:
                 subj = pair[1].decode("utf-8")
                 subj = subj[subj.find("<subj>")+6:subj.find("</subj>")]
-                print(pair[0].decode("utf-8").ljust(13,' '), ' ', subj)
+                print(pair[0].decode("utf-8").ljust(10,' '), ' ', subj)
 
 
 
