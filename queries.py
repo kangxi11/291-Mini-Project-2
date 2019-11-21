af1ec473129e03ddd24d13c395e5685c1358caa6
kangxi11 <rkang2009@gmail.com>
2019-11-21 16:38:51 -0700

    sds

--- queries.py
+++ queries.py
@@ -266,7 +266,7 @@ def main():
             print("Query did not match any records.")
         # print full output
         if setting:
-            print "Row ID       |       Subject Field"
+            print ("Row ID       |       Subject Field")
             for pair in output:
                 print(pair[0].decode("utf-8").ljust(13,' '), ' ', pair[1].decode("utf-8"))
         # print brief output
